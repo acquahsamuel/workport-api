0f856a0af81727f1ef22677de7785774bef7fbc2
acquahsamuel <acquahsamuel1996@gmail.com>
2020-08-13 16:12:51 +0000

    app.js feature

--- app.js
+++ app.js
@@ -18,14 +18,11 @@ app.set('view engine', 'handlebars');
 
 
 
-
 const home = require("./routes/home/index");
 const admin = require("./routes/admin/index");
 
 app.use("/" , home);
 app.use("/admin" ,admin);
-
-
 
 
 
