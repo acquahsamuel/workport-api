fb357327fec141c0faa63408eb4424995935d9b2
acquahsamuel <acquahsamuel1996@gmail.com>
2021-02-23 11:55:34 +0000

    app.js | entry

--- app.js
+++ app.js
@@ -1,37 +1,30 @@
-const path = require('path');
-const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-const colors = require('colors');
-const fileupload = require('express-fileupload');
-const cookieParser = require('cookie-parser');
-const mongoSanitize = require('express-mongo-sanitize');
-const helmet = require('helmet');
-const xss = require('xss-clean');
-const rateLimit = require('express-rate-limit');
-const hpp = require('hpp');
-const cors = require('cors');
-const connectDB = require('./config/db');
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const morgan = require("morgan");
+const fileupload = require("express-fileupload");
+const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
 
-// Load env vars
-dotenv.config({ path: './config/config.env' });
-
-// Connect to database
-connectDB();
-
-// Route files in 
-
+// Route files in
 const app = express();
 
 // Body parser
 app.use(express.json());
 
 // Cookie parser
+app.use(express.json({ limit: "15kb" }));
 app.use(cookieParser());
 
 // Dev logging middleware
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // File uploading
@@ -49,9 +42,10 @@ app.use(xss());
 // Rate limiting
 const limiter = rateLimit({
   windowMs: 10 * 60 * 1000, // 10 mins
-  max: 100
+  max: 100,
+  message: "Too many request from this IP, Please try again in an hour!"
 });
-app.use(limiter);
+app.use("/api", limiter);
 
 // Prevent http param pollution
 app.use(hpp());
@@ -59,14 +53,50 @@ app.use(hpp());
 // Enable CORS
 app.use(cors());
 
-
 /**Serving Static files  */
 app.use(express.static(`${__dirname}/public`));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.set("view engine", "pug");
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// Mount routers
+/**
+ * @Rendering_pages
+ */
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-// app.use(errorHandler);
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.get("/job-listing", (req, res) => {
+  res.render("job-listing");
+});
+
+app.get("/job-details", (req, res) => {
+  res.render("job-details");
+});
+
+app.get("/main", (req, res) => {
+  res.render("main");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+module.exports = app;
