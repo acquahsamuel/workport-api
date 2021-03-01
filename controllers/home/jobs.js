0c143dd8e31406c3c289c4f1d54a1910dc8cb9d7
acquahsamuel <acquahsamuel1996@gmail.com>
2021-03-01 12:05:32 +0000

    controller |-admin-|-index

--- controllers/home/jobs.js
+++ controllers/home/jobs.js
@@ -11,4 +11,10 @@ exports.getJobs = async (req, res, next) => {
   });
 };
 
-
+exports.createJob = async(req, res , next) => {
+  const job = await Job.create(req.body);
+  res.status(200).json({ 
+    success : true,
+    data : job
+  })
+}
