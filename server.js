28f85bb517a1549827f7741f467211be9a366881
acquahsamuel <acquahsamuel1996@gmail.com>
2021-03-01 13:39:51 +0000

    config |mongouri_prod

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const app = require('./app');
-
-process.on('uncaughtException' , err =>{
-  console.log('UNCAUGHT EXCEPTION Shutting down');
-  console.log(err.name, err.message);
-  process.exit(1);
-})
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config/config.env" });
 
 
-
-dotenv.config({path : './config/config.env'});
+const connectDB = require("./config/db");
+const app = require("./app");
 connectDB();
 
-const port = process.env.PORT || 5000;
-const server = app.listen(port, ()=>{
-  console.log(`App runing on port ${port}`)
-})
 
-process.on('unhandledRejection' , err =>{
-  console.log('UNHANDLED REJECTION Shutting down');
-  console.log(err.name , err.message);
-  server.close(() =>{
+process.on("uncaughtException", err => {
+  console.log("UNCAUGHT EXCEPTION Shutting down");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+
+const port = process.env.PORT || 5000;
+const server = app.listen(port, () => {
+  console.log(`App runing on port ${port}`);
+});
+
+process.on("unhandledRejection", err => {
+  console.log("UNHANDLED REJECTION Shutting down");
+  console.log(err.name, err.message);
+  server.close(() => {
     process.exit(1);
-  })
+  });
 });
