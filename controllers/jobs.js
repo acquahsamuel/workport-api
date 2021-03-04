9de5d020431ec4f6a2c874c0064c4086fa39157f
acquahsamuel <acquahsamuel1996@gmail.com>
2021-03-04 15:16:11 +0000

    reviews controller

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -5,11 +5,12 @@ const Job = require("../models/Job");
 // @route         POST /api/v1/job
 // @access        Private
 exports.createJob = async (req, res, next) => {
-  // const job = await Job.create(req.body);
+  const job = await Job.create(req.body);
+
   res.status(200).json({
     success: true,
     message : "Job Created",
-    // data: job
+    data: job
   });
 };
 
