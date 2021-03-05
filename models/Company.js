670be1d5a478df2d502198ac5c0b72f9b003165c
acquahsamuel <acquahsamuel1996@gmail.com>
2021-03-05 06:50:49 +0000

    Companies model

---
+++ models/Company.js
@@ -0,0 +1,110 @@
+const mongoose = require("mongoose");
+
+const JobSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  // company : mongoose.Types.ObjectId(),
+  // user : mongoose.Types.ObjectId(),
+
+  // company: {
+  //   type: mongoose.Schema.ObjectId,
+  //   ref: "Company",
+  //   required: true
+  // },
+  // user: {
+  //   type: mongoose.Schema.ObjectId,
+  //   ref: "User",
+  //   required: true
+  // },
+
+  // slug: String,
+  position: {
+    type: String,
+  },
+
+  companyName: {
+    type: String
+  },
+  locationAllowed: {
+    type: String
+  },
+
+  jobCategory: {
+    type: [String],
+    required: true,
+    enum: [
+      "Web Development",
+      "Design ",
+      " Product",
+      "Customer Service",
+      "Data",
+      "Sales",
+      "DevOps/SysAdmin",
+      "Business",
+      "Finance",
+      "Legal",
+      "Human Resources",
+      "Medical",
+      "Teaching",
+      "Everything Else"
+    ]
+  },
+
+  jobTags: {
+    type: [String],
+    required  : true
+  },
+  minimumSalary: {
+    type: Number,
+    maxlength : [15],
+    required  : true
+  },
+
+  maximumSalary: {
+    type: Number,
+    maxlength : [15],
+    required  : true
+  },
+  salaryInterval: {
+    type: [String]
+  },
+
+  jobStatus: {
+    type: [String],
+    required: true,
+    enum: ["Part time", "Full time", "Contract", "Internship"]
+  },
+
+
+  jobDescription: {
+    type: String,
+    required: [true, "Please add a description"],
+    required  : true
+  },
+  publicationDate: {
+    type: Date,
+    default: Date.now
+  },
+  applicationURL: {
+    type: String,
+    match: [
+      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+      "Please use a valid URL with HTTP or HTTPS"
+    ]
+  },
+
+  applyToEmail: {
+    type: String,
+    required: [true, "Please add an email"],
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please add a valid email"
+    ]
+  }
+});
+
+module.exports = mongoose.model("Job", JobSchema);
+
