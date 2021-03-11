b4da257333526d143ed6769344322edc97c3e34d
acquahsamuel <acquahsamuel1996@gmail.com>
2021-03-11 13:20:31 +0000

    utilities files

---
+++ utils/errorResponse.js
@@ -0,0 +1,11 @@
+const errorHandler = require("../middleware/error");
+
+class ErrorResponse extends Error {
+    constructor(message , statusCode ){
+        super(message);
+        this.statusCode = statusCode;
+    }
+}
+
+
+module.exports = errorHandler;
