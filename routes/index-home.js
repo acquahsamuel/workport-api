d02776c8e1e71e5ca7fb88b5f409be1eb7338838
acquahsamuel <acquahsamuel1996@gmail.com>
2021-03-12 03:36:24 +0000

    index-home

--- routes/index-home.js
+++ routes/index-home.js
@@ -1,45 +1,42 @@
 const express = require("express");
+const Job = require("../models/Job");
 const router = express.Router();
 
 /**
  * @Testing_
  */
-const Jobs = require('../models/Job');
-
+const Jobs = require("../models/Job");
 
 router.all("/*", (req, res, next) => {
   req.app.locals.layout = "home";
   next();
 });
 
-
-router.get("/", (req, res, next) => {
-  Jobs.find()
-  .then(job => {
-    res.render("home/index", {
-      job
+router.get("/", async (req, res, next) => {
+  const job = Jobs.find()
+    .then(job => {
+      res.render("home/index", {
+        job
+      });
+    })
+    .catch(error => {
+      console.log("There was something wrong" + error);
     });
-  })
-  .catch(error => {
-    console.log("There was something wrong" + error);
-  });
 });
+
 
 
 router.get("/job-details/:id", (req, res) => {
   Jobs.findById(req.params.id)
- .then(job => {
-   res.render("home/job-details", {
-     job
-   });
- })
- .catch(error => {
-   console.log("There was something wrong" + error);
- });
+    .then(job => {
+      res.render("home/job-details", {
+        job
+      });
+    })
+    .catch(error => {
+      console.log("There was something wrong" + error);
+    });
 });
-
-
-
 
 router.get("/about", (req, res) => {
   res.render("home/about");
@@ -52,8 +49,6 @@ router.get("/contact", (req, res) => {
 router.get("/job-listing", (req, res) => {
   res.render("home/job-listing");
 });
-
-
 
 router.get("/login", (req, res) => {
   res.render("home/login");
