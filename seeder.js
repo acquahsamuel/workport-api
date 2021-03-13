986141b96589b5ed331aeecb15c88d80b3ee2f84
acquahsamuel <acquahsamuel1996@gmail.com>
2021-03-13 15:53:54 +0000

    companies _data imported

--- seeder.js
+++ seeder.js
@@ -1,20 +1,27 @@
 const fs = require("fs");
 const dotenv = require("dotenv");
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
 const Job = require("./models/Job");
+const Company = require("./models/Company");
 
-// Connect to DB             
-dotenv.config({ path: "./config/config.env" });                                                                                                                                                                                                                                                                                                         
+// Connect to DB
+dotenv.config({ path: "./config/config.env" });
 connectDB();
 
 const jobs = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/jobs.json`, "utf-8")
 );
 
+const companies = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/compaies.json`, "utf-8")
+);
+
+
 const importData = async () => {
   try {
     await Job.create(jobs);
-    console.log("Data imported...");
+    await Company.create(companies);
+    await console.log("Data imported...");
     process.exit();
   } catch (err) {
     console.log(err);
@@ -36,4 +43,3 @@ if (process.argv[2] === "-i") {
 } else if (process.argv[2] === "-d") {
   deleteData();
 }
-
