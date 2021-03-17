d23c5c46f685c47e6143deede0a5d5652c874385
acquahsamuel <acquahsamuel1996@gmail.com>
2021-03-17 16:36:55 +0000

    server.js

--- server.js
+++ server.js
@@ -93,10 +93,16 @@ const server = app.listen(port, () => {
 });
 
 //@desc  Handle unhandled Rejection
-process.on("unhandledRejection", err => {
-  console.log("UNHANDLED REJECTION Shutting down");
-  console.log(err.name, err.message);
-  server.close(() => {
-    process.exit(1);
-  });
-});
+// process.on("unhandledRejection", err => {
+//   console.log("UNHANDLED REJECTION Shutting down");
+//   console.log(err.name, err.message);
+//   server.close(() => {
+//     process.exit(1);
+//   });
+// });
+
+
+
+
+
+
