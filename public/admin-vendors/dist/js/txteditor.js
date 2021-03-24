dce7eb42e39dfa0424aeb6679b8af907cda1f79d
acquahsamuel <acquahsamuel1996@gmail.com>
2021-03-24 06:31:35 +0000

    tinymce editor integratation

--- public/admin-vendors/dist/js/txteditor.js
+++ public/admin-vendors/dist/js/txteditor.js
@@ -1,3 +1,8 @@
 tinymce.init({
-  selector: '#mytextarea'
+  selector: '#mytextarea',
+  height : 300,
+  // plugins: 'lists',
+  toolbar: 'numlist bullist bold italic '
+  // toolbar : " bold italic strikethrough forecolor backcolor formatpainter | alignleft aligncenter alignright alignjustify | numlist bullist outdent indent | link insertfile image | removeformat | code | addcomment | checklist | casechange"
+  
 });
