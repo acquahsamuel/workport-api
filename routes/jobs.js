72c94775b30ca4f261f4c149cc898ed3b5c9393f
acquahsamuel <acquahsamuel1996@gmail.com>
2021-03-25 07:21:53 +0000

    jobs routes

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,26 +1,20 @@
-const express = require("express");
+const express = require('express');
+
 //@desc      from => jobs controller
 const {
-  createJob,
-  getJob,
-  getJobs,
-  updateJob,
-  deleteJob
-} = require("../controllers/jobs");
+    createJob,
+    getJob,
+    getJobs,
+    updateJob,
+    deleteJob,
+} = require('../controllers/jobs');
 const router = express.Router();
-                                           
-/*
-*
- * @desc  slash( / ) means route which is 
- * 
- * /api/v1/jobs
- * /api/v1/jobs/:id
- * 
-*/
 
+// @desc  slash( / ) means route which is   /api/v1/jobs - /api/v1/jobs/:id
 router.route('/').get(getJobs).post(createJob);
 router.route('/:id').get(getJob).put(updateJob).delete(deleteJob);
 
-
-
 module.exports = router;
+
+
+
