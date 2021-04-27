91164c38924d1d3d2295c316cfdc610c54293cc9
acquahsamuel <acquahsamuel1996@gmail.com>
2021-04-27 18:32:09 +0000

    User models instance  created

--- models/User.js
+++ models/User.js
@@ -42,7 +42,8 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-// Encrypted using bcrypt
+
+// Encrypt password using bcrypt
 UserSchema.pre('save', async function(next) {
   if (!this.isModified('password')) {
     next();
@@ -52,9 +53,35 @@ UserSchema.pre('save', async function(next) {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
-//Signed JWT authentication
-UserSchema.methods.getSignedJWTToken = function() {
-  return jwt.sign({id: this_id}, process.env.JWT);
+// Sign JWT and return
+UserSchema.methods.getSignedJwtToken = function() {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE
+  });
 };
+
+// Match user entered password to hashed password in database
+UserSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Generate and hash password token
+UserSchema.methods.getResetPasswordToken = function() {
+  // Generate token
+  const resetToken = crypto.randomBytes(20).toString('hex');
+
+  // Hash token and set to resetPasswordToken field
+  this.resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  // Set expire
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
+};
+
+
 
 module.exports = mongoose.model('User', UserSchema);
