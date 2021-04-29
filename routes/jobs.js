46b5a056078c29cd4759c3f9837b5a33dc0689bf
acquahsamuel <acquahsamuel1996@gmail.com>
2021-04-29 07:00:46 +0000

    jobs users routes

--- routes/jobs.js
+++ routes/jobs.js
@@ -2,20 +2,25 @@ const express = require('express');
 
 //@desc      from => jobs controller
 const {
-    createJob,
-    getJob,
-    getJobs,
-    updateJob,
-    deleteJob,
+  createJob,
+  getJob,
+  getJobs,
+  updateJob,
+  deleteJob,
 } = require('../controllers/jobs');
 const router = express.Router();
 
-// @desc  slash( / ) means route which is   /api/v1/jobs - /api/v1/jobs/:id
+const Job = require('../models/Job');
+const advancedResults = require('../middleware/advancedResults');
+
+
+/*
+ *
+ * @des varification here
+ *
+ *  
+ */
 router.route('/').get(getJobs).post(createJob);
 router.route('/:id').get(getJob).put(updateJob).delete(deleteJob);
 
 module.exports = router;
-
-
-
-
--- routes/users.js
+++ routes/users.js
@@ -13,13 +13,16 @@ const router = express.Router();
 
 /*
  * @desc  slash( / ) means route which is 
- * 
  * /api/v1/user
  * /api/v1/users/:id
- * 
+ *
+ *  
 */
 
 router.route("/").get(getUsers).post(createUser);
 router.route("/:id").get(getUser).put(updateUser).delete(deleteUser);
 
 module.exports = router;
+
+
+
