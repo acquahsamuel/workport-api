ca25f2902649f51213808109ee5ca8212df2323f
acquahsamuel <acquahsamuel1996@gmail.com>
2021-06-21 15:45:16 +0000

    home routes

---
+++ routes/home-v.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const {
+  getHomeIndex,
+  getHomeJobDetails,
+  getHomeAbout,
+  getHomeBlog,
+  getHomeJobListing,
+  getHomeLogin,
+  getHomeRegister,
+  getHomeSingleBlog,
+  getHome404
+} = require("../controllers/home");
+const router = express.Router();
+
+router.get("/", getHomeIndex);
+router.get("/job-details/:id", getHomeJobDetails);
+router.get("/about", getHomeAbout);
+router.get("/blog", getHomeBlog);
+router.get("/job-listing", getHomeJobListing);
+router.get("/login", getHomeLogin);
+router.get("/register", getHomeRegister);
+router.get("/single-blog", getHomeSingleBlog);
+router.get("/error-404" , getHome404);
+
+module.exports = router;
+
+
+
+
