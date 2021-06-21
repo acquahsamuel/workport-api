147ec5a18444693d1c5ca46eaa7b1b4688e469e0
acquahsamuel <acquahsamuel1996@gmail.com>
2021-06-21 15:44:51 +0000

    admin routes

---
+++ routes/admin-v.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const {
+  getAdminIndex,
+  getAdminPostJob,
+  getAdminManageJob,
+  getAdminInvoice,
+} = require("../controllers/admin");
+
+const router = express.Router();
+
+router.get("/index", getAdminIndex);
+router.get("/post-job", getAdminPostJob);
+router.get("/manage-job", getAdminManageJob);
+router.get("/invoice", getAdminInvoice);
+
+module.exports = router;
