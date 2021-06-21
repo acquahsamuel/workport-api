16a7e9b739ae221d50dcb466ad54f08b3b59c639
acquahsamuel <acquahsamuel1996@gmail.com>
2021-06-21 15:43:38 +0000

    admin controllers

---
+++ controllers/admin.js
@@ -0,0 +1,48 @@
+const Job = require("../models/Job");
+const express = require("express");
+const router = express.Router();
+
+// @desc          Delete a single Company
+// @route         www.workport.com/index
+// @access        Private
+exports.getAdminIndex = async (req, res, next) => {
+  res.status(200).render("admin/index", {
+    pageTitle: "Dashboard",
+    path: "/admin/index",
+  });
+};
+
+// @desc          Delete a single Company
+// @route         www.workport.com/index
+// @access        Private
+exports.getAdminPostJob = async (req, res, next) => {
+  const postJob = await Job.create(req.body.title);
+  console.log(postJob);
+
+  res.status(200).render("admin/post-job", {
+    pageTitle: "Post Job",
+    path: "/admin/post-job",
+  });
+};
+
+// @desc          Delete a single Company
+// @route         www.workport.com/index
+// @access        Private
+exports.getAdminManageJob = async (req, res, next) => {
+  res.status(200).render("admin/manage-job", {
+    pageTitle: "Manage Job",
+    path: "/admin/manage-job",
+  });
+};
+
+// @desc          Delete a single Company
+// @route         www.workport.com/index
+// @access        Private
+exports.getAdminInvoice = async (req, res, next) => {
+  res.status(200).render("admin/invoice", {
+    pageTitle: "Invoice",
+    path: "/admin/invoice",
+  });
+};
+
+// module.exports = router;
