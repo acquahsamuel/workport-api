0c24ae2dfec8a6a81044f369293dc89f9a9767b4
acquahsamuel <acquahsamuel1996@gmail.com>
2021-06-22 07:09:13 +0000

    routes/admin

--- routes/admin.js
+++ routes/admin.js
@@ -4,12 +4,15 @@ const {
   getAdminPostJob,
   getAdminManageJob,
   getAdminInvoice,
+  postAdminJob,
 } = require("../controllers/admin");
 
 const router = express.Router();
 
 router.get("/index", getAdminIndex);
 router.get("/post-job", getAdminPostJob);
+
+router.post("/post-job", postAdminJob);
 router.get("/manage-job", getAdminManageJob);
 router.get("/invoice", getAdminInvoice);
 
