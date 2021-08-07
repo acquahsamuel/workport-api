24efcc96b3bcf4a507bf8dae6cb37ec19beca386
acquahsamuel <acquahsamuel1996@gmail.com>
2021-08-07 15:03:18 +0000

    config.env file

--- controllers/home.js
+++ controllers/home.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 const asyncHandler = require("../middleware/async");
 
+
 // @desc          Get Job listings
 // @route         www.workport.com/index
 // @access        Public
