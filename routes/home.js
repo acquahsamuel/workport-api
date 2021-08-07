85f52522ed09118cce675c9befc3976e0dc896ca
acquahsamuel <ackomprince39@gmail.com>
2021-08-07 14:38:35 +0000

    routes: implementation for home routes

--- routes/home.js
+++ routes/home.js
@@ -8,22 +8,18 @@ const {
   getHomeLogin,
   getHomeRegister,
   getHomeSingleBlog,
-  getHome404
+  getHome404,
 } = require("../controllers/home");
 const router = express.Router();
 
 router.get("/", getHomeIndex);
-router.get("/job-details/:id", getHomeJobDetails);
+router.get("/job-details/:jobId", getHomeJobDetails);
 router.get("/about", getHomeAbout);
 router.get("/blog", getHomeBlog);
 router.get("/job-listing", getHomeJobListing);
 router.get("/login", getHomeLogin);
 router.get("/register", getHomeRegister);
 router.get("/single-blog", getHomeSingleBlog);
-router.get("/error-404" , getHome404);
+router.get("/error-404", getHome404);
 
 module.exports = router;
-
-
-
-
