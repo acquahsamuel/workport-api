da8bf4f865d7cf178c6d63ecd2ba145c9210b493
acquahsamuel <acquahsamuel1996@gmail.com>
2021-09-05 21:16:02 +0000

    [WIP] +> controllers, admin , home

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,49 +1,43 @@
-const Job = require("../models/Job");
+const Job = require('../models/Job')
 // const Company = require("../models/Company");
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-// @desc          Delete a single Company
-// @route         www.workport.com/index
 // @access        Private
 exports.getAdminIndex = async (req, res, next) => {
-  await res.status(200).render("admin/index", {
-    pageTitle: "Dashboard",
-    path: "/admin/index",
-  });
-};
+  await res.status(200).render('admin/index', {
+    pageTitle: 'Dashboard',
+    path: '/admin/index',
+  })
+}
 
-// @desc          Get post job
-// @route         www.workport.com/index
 // @access        Private
 exports.getAdminPostJob = (req, res, next) => {
-  res.status(200).render("admin/post-job", {
-    pageTitle: "Post Job",
-    path: "/admin/post-job",
-  });
-};
+  res.status(200).render('admin/post-job', {
+    pageTitle: 'Post Job',
+    path: '/admin/post-job',
+  })
+}
 
-// @desc          Post a single Job
-// @route         www.workport.com/index
 // @access        Private
 exports.postAdminJob = (req, res, next) => {
-  const position = req.body.position;
-  const locationAllowed = req.body.locationAllowed;
-  const jobStatus = req.body.jobStatus;
-  const jobCategory = req.body.jobCategory;
-  const jobTags = req.body.jobTags;
-  const minimumSalary = req.body.minimumSalary;
-  const maximumSalary = req.body.maximumSalary;
-  const currency = req.body.currency;
-  const salaryInterval = req.body.salaryInterval;
-  const jobDescription = req.body.jobDescription;
-  const applicationURL = req.body.applicationURL;
-  const applyToEmail = req.body.applyToEmail;
+  const position = req.body.position
+  const locationAllowed = req.body.locationAllowed
+  const jobStatus = req.body.jobStatus
+  const jobCategory = req.body.jobCategory
+  const jobTags = req.body.jobTags
+  const minimumSalary = req.body.minimumSalary
+  const maximumSalary = req.body.maximumSalary
+  const currency = req.body.currency
+  const salaryInterval = req.body.salaryInterval
+  const jobDescription = req.body.jobDescription
+  const applicationURL = req.body.applicationURL
+  const applyToEmail = req.body.applyToEmail
 
-  const companyName = req.body.companyName;
-  const companyLogo = req.body.companyLogo;
-  const companyTwitter = req.body.companyTwitter;
-  const companyEmail = req.body.companyEmail;
+  const companyName = req.body.companyName
+  const companyLogo = req.body.companyLogo
+  const companyTwitter = req.body.companyTwitter
+  const companyEmail = req.body.companyEmail
 
   const job = new Job({
     position: position,
@@ -65,51 +59,42 @@ exports.postAdminJob = (req, res, next) => {
       companyTwitter: companyTwitter,
       companyEmail: companyEmail,
     },
-  });
+  })
 
   job
     .save()
     .then((result) => {
-      console.log("Job Created");
-      res.redirect("/");
+      console.log('Job Created')
+      res.redirect('/')
     })
     .catch((err) => {
-      console.log(err);
-    });
-};
+      console.log(err)
+    })
+}
 
-// @desc          Get  a single Company
-// @route         www.workport.com/index
 // @access        Private
 exports.getAdminManageJob = async (req, res, next) => {
-  const jobs = await Job.find({});
-  await res.status(200).render("admin/manage-job", {
-    pageTitle: "Manage Job",
-    path: "/admin/manage-job",
-    jobs
-  });
-};
+  const jobs = await Job.find({})
+  await res.status(200).render('admin/manage-job', {
+    pageTitle: 'Manage Job',
+    path: '/admin/manage-job',
+    jobs,
+  })
+}
 
-
-// @desc          Get  a  Job  
-// @route         www.workport.com/index
 // @access        Private
 exports.getAdminManageSingleJob = async (req, res, next) => {
   // const jobs = await Job.findById();
-  await res.status(200).render("admin/manage-job", {
-    pageTitle: "Manage Job",
-    path: "/admin/manage-job",
-  });
-};
+  await res.status(200).render('admin/manage-job', {
+    pageTitle: 'Manage Job',
+    path: '/admin/manage-job',
+  })
+}
 
-
-
-// @desc          Delete a single Company
-// @route         www.workport.com/index
 // @access        Private
 exports.getAdminInvoice = async (req, res, next) => {
-  await res.status(200).render("admin/invoice", {
-    pageTitle: "Invoice",
-    path: "/admin/invoice",
-  });
-};
+  await res.status(200).render('admin/invoice', {
+    pageTitle: 'Invoice',
+    path: '/admin/invoice',
+  })
+}
--- controllers/home.js
+++ controllers/home.js
@@ -1,67 +1,65 @@
-const Job = require("../models/Job");
-const express = require("express");
-const asyncHandler = require("../middleware/async");
+const express = require('express')
+const Job = require('../models/Job')
+const asyncHandler = require('../middleware/async')
+const ErrorResponse = require('../utils/errorResponse');
 
 
-// @desc          Get Job listings
-// @route         www.workport.com/index
 // @access        Public
 exports.getHomeIndex = asyncHandler(async (req, res, next) => {
-  const jobs = await Job.find({});
-  res.status(200).render("home/index", {
-    pageTitle: "Homepage",
+  const jobs = await Job.find({})
+  const totalJobs = jobs.length;
+  res.status(200).render('home/index', {
+    pageTitle: 'Homepage',
     jobs: jobs,
-    path: "/home/index",
-  });
-});
+    path: '/home/index',
+    totalJobs
+  })
+})
 
 
-
-// @desc          Get Job listings
-// @route         www.workport.com/job-details
 // @access        Public
 exports.getHomeJobDetails = asyncHandler(async (req, res, next) => {
-  const slug = req.params.slug;
-  const job = await Job.findById(slug);
-  // const job = await Job.findById(jobId);
+  const job = await Job.findOne({ slug: req.params.slug })
+
+  if (!job) {
+    return next(
+      new ErrorResponse(`Job not found with id of ${req.params.id}`, 404),
+    )
+  }
 
   res.status(200).render(`home/job-details`, {
-    pageTitle: "Job Details",
-    path: "/home/job-details",
+    pageTitle: 'Job Details',
+    path: '/home/job-details',
     job,
-    // company
-  });
-});
+  })
+})
 
-
-// @desc          Get Job listings
-// @route         www.workport.com/job-listings
 // @access        Public
 exports.getHomeJobListing = asyncHandler(async (req, res, next) => {
   const job = await Job.find();
+  const totalJobs = job.length;
   res.status(200).render(`home/job-listing`, {
-    pageTitle: "Job Listings",
-    path: "/home/job-listing",
+    pageTitle: 'Job Listings',
+    path: '/home/job-listing',
     job,
-  });
-});
+    totalJobs
+  })
+})
 
-// @desc          Get Job listings
-// @route         www.workport.com/login
+
 // @access        Public
 exports.getHomeLogin = asyncHandler(async (req, res, next) => {
-  await res.status(200).render("home/login", {
-    pageTitle: "Account Login",
-    path: "/home/login",
-  });
-});
+  await res.status(200).render('home/login', {
+    pageTitle: 'Account Login',
+    path: '/home/login',
+  })
+})
 
-// @desc          Get Job listings
-// @route         www.workport.com/register
+
 // @access        Public
 exports.getHomeSignup = asyncHandler(async (req, res, next) => {
-  await res.status(200).render("home/signup", {
-    pageTitle: "Account Registration",
-    path: "/home/signup",
-  });
-});
+  await res.status(200).render('home/signup', {
+    pageTitle: 'Account Registration',
+    path: '/home/signup',
+  })
+})
