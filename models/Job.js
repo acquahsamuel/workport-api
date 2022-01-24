ede1ff38963353cea6882e02c68b47a639e789df
acquahsamuel <iamsamuelacquah@gmail.com>
2022-01-24 13:22:42 +0000

    modelx jobs email Unique bug fix

--- models/Job.js
+++ models/Job.js
@@ -1,94 +1,84 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
 
-const JobSchema = new mongoose.Schema({
-  position: {
-    type: String,
-  },
+const JobSchema = new mongoose.Schema(
+  {
+    position: {
+      type: String,
+    },
 
-  locationAllowed: {
-    type: String,
-  },
+    locationAllowed: {
+      type: String,
+    },
 
-  jobStatus: {
-    type: String,
-    required: true,
-  },
+    jobStatus: {
+      type: String,
+      required: true,
+    },
 
-  jobCategory: {
-    type: String,
-    required: true,
-  },
+    jobCategory: {
+      type: String,
+      required: true,
+    },
 
-  jobSearchTags: {
-    type: [String],
-    required: [true, "Please add jobs tags"],
-  },
+    jobSearchTags: {
+      type: [String],
+      required: [true, "Please add jobs tags"],
+    },
 
-  minimumSalary: {
-    type: Number,
-    minlength: [5],
-    required: true,
-  },
-  slug: String,
+    currency: {
+      type: String,
+      required: true,
+    },
 
-  maximumSalary: {
-    type: Number,
-    maxlength: [15],
-    required: true,
-  },
+    jobDescription: {
+      type: String,
+      required: [true, "Please add a description"],
+    },
 
-  currency: {
-    type: String,
-    required: true,
-  },
+    applicationURL: {
+      type: String,
+      match: [
+        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+        "Please use a valid URL with HTTP or HTTPS",
+      ],
+    },
 
-  jobDescription: {
-    type: String,
-    required: [true, "Please add a description"],
-  },
+    // type of string = requires only string no number
+    applyToEmail: {
+      type: String,
+      required: [true, "Please add an email"],
+      // unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please add a valid email",
+      ],
+    },
+    datePosted: {
+      type: Date,
+      default: Date.now,
+    },
 
-  applicationURL: {
-    type: String,
-    match: [
-      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
-      "Please use a valid URL with HTTP or HTTPS",
-    ],
+    company: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Company",
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
-
-  // type of string = requires only string no number
-  applyToEmail: {
-    type: String,
-    required: [true, "Please add an email"],
-    unique: true,
-    match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      "Please add a valid email",
-    ],
-  },
-  datePosted: {
-    type: Date,
-    default: Date.now,
-  },
-
-  company: {
-    type: mongoose.Schema.ObjectId,
-    ref: "Company",
-    required: true,
-  },
-  user: {
-    type: mongoose.Schema.ObjectId,
-    ref: "User",
-    required: true,
-  },
-}, {
-  toJSON: {
-    virtuals: true,
-  },
-  toObject: {
-    virtuals: true,
-  },
-});
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+  }
+);
 
 JobSchema.pre("save", function (next) {
   this.slug = slugify(this.position, {
